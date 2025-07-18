d7bf73993c9dd26d64ff0b4d220b5b7728cf8823
khalilks5470 <khalilks547@gmail.com>
2025-07-18 17:52:31 +0500

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -1,27 +1,32 @@
-const fs = require("fs");
 module.exports.config = {
-  name: "admin2",
-  version: "1.0.1",
-  hasPermssion: 0,
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩", 
-  description: "hihihihi",
-  commandCategory: "no prefix",
-  usages: "admin",
-    cooldowns: 5, 
+    name: "admin",
+    version: "1.0.1", 
+    hasPermssion: 0,
+    credits: "Shaan",
+    description: "Shaan Tech 49",
+    commandCategory: "...",
+    cooldowns: 1,
+    dependencies: 
+    {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
 };
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+var link =["https://i.imgur.com/DYuNnAi.jpeg", 
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  if (event.body.indexOf("ADMIN")==0 || event.body.indexOf("Admin")==0 || event.body.indexOf("/Admin")==0 || event.body.indexOf("#admin")==0) {
-    var msg = {
-        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
-        attachment: 
-fs.createReadStream(__dirname + `/noprefix/profile.png`)
-      }
-      api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
-    }
-  }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
+            "https://i.imgur.com/DYuNnAi.jpeg", 
 
-  }
+"https://i.imgur.com/DYuNnAi.jpeg",
+
+            "https://i.imgur.com/DYuNnAi.jpeg"];
+
+var callback = () => api.sendMessage({body:`𝗗𝗢 𝗡𝗢𝗧 𝗧𝗥𝗨𝗦𝗧 𝗧𝗛𝗘 𝗕𝗢𝗧 𝗢𝗣𝗘𝗥𝗔 𝗧𝗢𝗥\n
+------------------------------------------------\n𝗡𝗮𝗺𝗲       : 𝗛𝗔𝗠𝗭𝗔 𝗞𝗜𝗡𝗚\n𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : 𝗛𝗔𝗠𝗭𝗔𝗔𝗟𝗜\n𝗥𝗲𝗹𝗶𝗴𝗶𝗼𝗻    : (𝗜𝘀𝗹𝗮𝗺)\n𝗣𝗲𝗿𝗺𝗮𝗻𝗲𝗻𝘁 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : (𝗟𝗔𝗛𝗢𝗥𝗘)\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 :𝗟𝗔𝗛𝗢𝗥𝗘, 𝗣𝗮𝗸𝗶𝘀𝘁𝗮𝗻\n𝗚𝗲𝗻𝗱𝗲𝗿     : (𝗠𝗮𝗹𝗲)\n𝗔𝗴𝗲            :  (19)\n𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻𝘀𝗵𝗶𝗽 : (𝗦𝗶𝗻𝗴𝗹𝗲)\n𝗪𝗼𝗿𝗸         : 𝙎𝙩𝙪𝙙𝙮\n𝗚𝗺𝗮𝗶𝗹        ✅𝗡𝗔𝗛𝗜 𝗛𝗔𝗜\n𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :  wa.me/03206700336\n𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺  : yaad nahi hai\n𝗙𝗯 𝗹𝗶𝗻𝗸   : https://www.facebook.com/share/1BdXVFf7dW/
+`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
+   };
